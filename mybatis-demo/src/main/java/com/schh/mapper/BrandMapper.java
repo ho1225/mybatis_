f5b755dc9c222422e0bbbd0dc160ceee051cd424
MSI\hosiu <hosiuchuen05@gmail.com>
2024-12-02 18:16:07 +0800

    add static and dynamic sql query for mybatis

--- mybatis-demo/src/main/java/com/schh/mapper/BrandMapper.java
+++ mybatis-demo/src/main/java/com/schh/mapper/BrandMapper.java
@@ -1,13 +1,30 @@
 package com.schh.mapper;
 
 import com.schh.pojo.Brand;
-import com.schh.pojo.User;
+
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
 
     List<Brand> selectAll();
 
+
     Brand selectById(int id);
+
+    /**
+     * conditional query
+     * 1. seperated
+     * 2. object
+     * 3. map
+
+     */
+//    List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+
+//    List<Brand> selectByCondition(Brand brand);
+
+    List<Brand> selectByCondition(Map map);
+
+    List<Brand> selectByConditionSingle(Brand brand);
 }
--- mybatis-demo/src/test/java/com/schh/test/MyBatisTest.java
+++ mybatis-demo/src/test/java/com/schh/test/MyBatisTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
@@ -27,6 +29,101 @@ public class MyBatisTest {
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
 
         List<Brand> brands = brandMapper.selectAll();
+        System.out.println(brands);
+
+
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void testSelectById() throws IOException {
+        int id = 1;
+
+        String resource = "mybatis-config.xml";
+
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        Brand brand = brandMapper.selectById(id);
+        System.out.println(brand);
+
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectByCondition() throws IOException {
+
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+//        Brand brand = new Brand();
+//        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+
+        Map map = new HashMap();
+//        map.put("status" ,status);
+        map.put("companyName", companyName);
+//        map.put("brandName", brandName);
+
+
+
+
+        String resource = "mybatis-config.xml";
+
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+//        List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+//        List<Brand> brands = brandMapper.selectByCondition(brand);
+        List<Brand> brands = brandMapper.selectByCondition(map);
+        System.out.println(brands);
+
+
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void testSelectByConditionSingle() throws IOException {
+
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+
+        String resource = "mybatis-config.xml";
+
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+
+        List<Brand> brands = brandMapper.selectByConditionSingle(brand);
         System.out.println(brands);
 
 
